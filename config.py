e9392450e41ab830ad91c518942193c4eae06c21
Luffy <120936732+anime91eki@users.noreply.github.com>
2023-06-30 21:03:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "20421248")
+    API_HASH  = os.environ.get("API_HASH", "90da70b17365b3709b8a0346a7749ce7")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6332284306:AAFRuWy0bEMrn_YFuVUeQqgs4pniYVmLhQs") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Abhi12:abhi12@cluster0.bdue3ap.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/df63377eb4f9ef35ad70a.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5009250822 1320302220').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "manga_Guardians") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001802708543"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
